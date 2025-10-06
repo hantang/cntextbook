3bbc69b60b96a1f9b9872aee42f41a31cf01e12c
hantang <34468720+hantang@users.noreply.github.com>
2025-10-06 18:23:46 +0800

    Add mkdocs

---
+++ docs/static/js/remove-space.js
@@ -0,0 +1,28 @@
+/* 删除中文和中文标点之间的空格*/
+(function () {
+  // [\u4e00-\u9fff\u3000-\u303f\uff00-\uffef“”‘’]
+  const chineseChars = "[\\p{Script=Han}\\u3000-\\u303F\\uFE10–\\uFE1F\\uFF00-\\uFFEF“”‘’…•·]";
+  const regex = new RegExp(
+    `(?<=${chineseChars})\\s+(?=${chineseChars})|^\\s+|\\s+$`,
+    "gu"
+  );
+
+  function cleanChineseSpaces(container) {
+    function cleanTextNodes(node) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        node.nodeValue = node.nodeValue.replace(regex, "");
+      } else {
+        node.childNodes.forEach(cleanTextNodes);
+      }
+    }
+    cleanTextNodes(container);
+  }
+
+  // MkDocs Material SPA 事件，每次页面加载都会触发
+  document$.subscribe(() => {
+    const content = document.querySelector(".md-content");
+    if (content) {
+      cleanChineseSpaces(content);
+    }
+  });
+})();
