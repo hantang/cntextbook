1964d0cd4aacc80f64543cc5f6125fc8060814a8
hantang <34468720+hantang@users.noreply.github.com>
2025-10-06 19:37:41 +0800

    Update

--- docs/static/js/heti.js
+++ docs/static/js/heti.js
@@ -1,2 +1,6 @@
-const heti = new Heti('.heti');
-heti.autoSpacing(); // 自动进行中西文混排美化和标点挤压
+(() => {
+  document$.subscribe(() => {
+    const heti = new Heti('.heti');
+    heti.autoSpacing(); // 自动进行中西文混排美化和标点挤压
+  });
+})();
